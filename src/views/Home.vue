56f4fbcdeea1f1313389c72dafbd77d0f6e651e2
Tesar Gantara Suherman <tgantara@gmail.com>
2020-07-19 19:10:11 +0700

    upload

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Slider />
+    <Banner />
+    <Insta />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Slider from '@/components/Slider.vue';
+import Banner from '@/components/Banner.vue';
+import Insta from '@/components/Insta.vue';
+import Partner from '@/components/Partner.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Slider,
+    Banner,
+    Insta,
+    Partner,
+    Footer
+  }
+};
+</script>
